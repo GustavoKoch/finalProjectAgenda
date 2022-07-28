c54c48061e45fc119d58226746a288edfd47591a
gigi <giovannaggs@gmail.com>
2022-07-28 09:25:47 +0200

    wednesday afternoon

--- src/CalendarView.js
+++ src/CalendarView.js
@@ -3,12 +3,15 @@ import Calendar from "react-calendar";
 import "./App.css";
 import "react-calendar/dist/Calendar.css";
 
+
 function CalendarView() {
     const [date, setDate] = useState([
         new Date(2021, 6, 1),
         new Date(2021, 6, 10)
     ]);
-
+    const saveEvent = (e) => {
+        e.preventDefault()
+    }
     return (
         <div className="app">
             <h1 className="text-center">Calendar</h1>
@@ -17,14 +20,18 @@ function CalendarView() {
             </div>
             {/* if we have 2 dates */}
             {/* {date.length > 1 && */}
-            <p className="text-center">
+            <form onSubmit={saveEvent} className="text-center">
+                <span className="bold">Add Event:</span> <input />
+                <br />
                 <span className="bold">Start:</span> {date[0].toDateString()}
-                &nbsp;|&nbsp;
+                &nbsp;&nbsp;
+                <br />
                 <span className="bold">End:</span> {date[1].toDateString()}
-                <span className="bold">Description:</span> <input />
+                <br />
+                <button className="submit">Create</button>
 
-            </p>
-        </div>
+            </form >
+        </div >
     );
 }
 
