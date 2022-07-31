8a74a5528dea9218daf0e23bb66afa143d34d403
gigi <giovannaggs@gmail.com>
2022-07-31 22:09:50 +0200

    todo list update

--- src/Todo.js
+++ src/Todo.js
@@ -66,7 +66,9 @@ React.useEffect(() => {
 
   return (
     <div className="App">
-      <h1> Todo List</h1>
+      <h1> Todo List
+        
+      </h1>
     <form onSubmit={handleSubmit}>
       <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo}/>
       <button type="submit"> ➕</button>
